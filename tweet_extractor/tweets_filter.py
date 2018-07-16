ce48614a703f3f26d8e85a4ea57d7c13fb4b630c
Nafseer Kadiyaravida <nafseerk@gmail.com>
2018-07-15 21:55:38 -0400

    Added caching and time delay for geocoding

--- tweet_extractor/tweets_filter.py
+++ tweet_extractor/tweets_filter.py
@@ -1,8 +1,16 @@
 import json
 import datetime
 import time
+import geopy.geocoders
 from geopy.exc import GeocoderTimedOut
 from geopy.geocoders import Nominatim
+from tweet_extractor.location_cache import get_location_cache, save_location_cache
+
+# Set timeout period for Geocoder API
+geopy.geocoders.options.default_timeout = 7
+
+# For duplicate checking of tweets
+unique_tweets = {}
 
 
 # Convert twitter date to posix timestamp
@@ -14,23 +22,58 @@ def get_timestamp_from_date(date_string):
 
 
 # Get only require fields from a raw tweet
-def get_required_fields(input_json_string):
+def get_required_fields(input_json_string, location_cache):
 
     # JSON string to JSON object
     in_json = json.loads(input_json_string)
 
-    try:
-        out_json = {}
+    # Duplicate checking for tweets
+    if in_json['id'] in unique_tweets:
+        print('Duplicate tweet. Ignoring')
+        return None
+    unique_tweets[in_json['id']] = True
 
-        out_json['id'] = in_json['id']
+    try:
+        out_json = {'id': in_json['id']}
 
         # Find latitude and longitude from user location field
-        geo_locator = Nominatim()
-        location = geo_locator.geocode(in_json['user']['location'])
-        if not location:
+        if in_json['user']['location']:
+            twitter_location = in_json['user']['location']
+
+            # If location not cached, use geocoding API
+            if twitter_location in location_cache:
+                print('Location {0} cached as {1}, {2}'.format(twitter_location,
+                                                               location_cache[twitter_location][0],
+                                                               location_cache[twitter_location][1]))
+
+                out_json['latitude'] = location_cache[twitter_location][0]
+                out_json['longitude'] = location_cache[twitter_location][1]
+            else:
+                print('Location not cached. Fetching from GeoCoder')
+                geo_locator = Nominatim()
+
+                try:
+                    location = geo_locator.geocode(twitter_location)
+                    print('Geocoder returned ({0}, {1}) for location {2}'.format(location.latitude,
+                                                                                 location.longitude,
+                                                                                 twitter_location))
+                    # Sleeping to prevent HTTP Rate limiting by GeoCoder
+                    time.sleep(10)
+
+                    # If geocoder didn't find location, return None
+                    if not location:
+                        return None
+                    location_cache[twitter_location] = (location.latitude, location.longitude)
+                    out_json['latitude'] = location_cache[twitter_location][0]
+                    out_json['longitude'] = location_cache[twitter_location][1]
+
+                except Exception as e:
+                    print('Exception while getting required fields:', str(e))
+                    return None
+
+        # If no location in tweet, return None
+        else:
             return None
-        out_json['latitude'] = location.latitude
-        out_json['longitude'] = location.longitude
 
         # Convert twitter date to timestamp
         out_json['timestamp'] = get_timestamp_from_date(in_json['created_at'])
@@ -52,20 +95,43 @@ def filter_tweets(input_file):
     # Assumes that the input_file has .json extension
     output_file = input_file[:-5] + '_filtered.json'
 
-    with open(output_file, 'w') as out_file:
-        filter_tweet_count = 0
-        for tweet in open(input_file, 'r'):
-            filtered_tweet = get_required_fields(tweet)
-            # print(filtered_tweet)
-            if filtered_tweet is not None:
-                out_file.write(filtered_tweet)
-                out_file.write('\n')
-                filter_tweet_count += 1
+    # Load the location_cache if exists
+    location_cache = get_location_cache()
+    print('Loaded location cache of size', len(location_cache))
 
-                if filter_tweet_count % 100 == 0:
-                    print('Filtered {0} tweets'.format(filter_tweet_count))
+    # Create output file
+    open(output_file, 'w', encoding='utf-8')
+
+    results = []
+    filter_tweet_count = 0
+    for i, tweet in enumerate(open(input_file, 'r', encoding='utf-8')):
+        filtered_tweet = get_required_fields(tweet, location_cache)
+        print(filtered_tweet)
+        if filtered_tweet is not None:
+            results.append(filtered_tweet)
+            filter_tweet_count += 1
+
+            if filter_tweet_count % 50 == 0:
+                print('Filtered {0} tweets from {1} tweets'.format(filter_tweet_count, i+1))
+
+                # Save location cache to file
+                save_location_cache(location_cache)
+
+                # Save results so far to file
+                with open(output_file, 'a', encoding='utf-8') as out_file:
+                    print('Saving {0} to file'.format(len(results)))
+                    for row in results:
+                        out_file.write(row)
+                        out_file.write('\n')
+                    results = []
+
+    with open(output_file, 'a', encoding='utf-8') as out_file:
+        print('Saving {0} to file'.format(len(results)))
+        for row in results:
+            out_file.write(row)
+            out_file.write('\n')
 
 
 if __name__ == '__main__':
-    raw_twitter_data_file = '/Users/apple/twitter_data/TRUMP/2018-07-08_historic.json'
+    raw_twitter_data_file = '/Users/apple/twitter_data/TRUDEUA/2018-07-10_historic.json'
     filter_tweets(raw_twitter_data_file)
